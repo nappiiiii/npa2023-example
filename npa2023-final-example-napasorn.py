f42e720e49771109be59fb51c33a6757c2e969f5
65070115 <65070115@kmitl.ac.th>
2024-10-22 23:38:28 +0700

    add roomId line27

--- npa2023-final-example-napasorn.py
+++ npa2023-final-example-napasorn.py
@@ -24,7 +24,7 @@ accessToken = "Bearer ZDdjNTc0NTQtYjY2My00ZGNiLTlhNzUtZjE1NTk4NDdiMWIwM2Y1MGUyND
 # 3. Prepare GetParameters to get the latest message for messages API.
 
 # Defines a variable that will hold the roomId 
-roomIdToGetMessages = "<!!!REPLACEME with roomID of the NPA2023 Webex Teams room!!!>" 
+roomIdToGetMessages = "Y2lzY29zcGFyazovL3VzL1JPT00vNTFmNTJiMjAtNWQwYi0xMWVmLWE5YTAtNzlkNTQ0ZjRkNGZi" 
 
 while True:
     # always add 1 second of delay to the loop to not go over a rate limit of API calls
